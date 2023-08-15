1504f04907b9cc0bcb7513c9fe9cd9af07a0631c
손성준 <sonsj97@gmail.com>
2023-08-16 01:20:08 +0900

    DB connect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,42 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
-    <>
-    <main>newsproject</main>
-    </>
-  );
+    const [news, setNews] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        async function fetchNews() {
+            try {
+                const response = await axios.get('http://localhost:5000/api/news');
+                setNews(response.data);
+                setLoading(false);
+            } catch (err) {
+                setError(err.message);
+                setLoading(false);
+            }
+        }
+
+        fetchNews();
+    }, []);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div>
+            <h1>News List</h1>
+            <ul>
+                {news.map(item => (
+                    <li key={item.id}>
+                        <h2>{item.title}</h2>
+                        <p>{item.content}</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default App;
---
+++ my-app/src/server/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const {Pool} = require('pg');
+const cors = require('cors');
+
+const app = express();
+const PORT = 5000;
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "postgres",
+    password: "qwaszx2689!",
+    port: 5432
+});
+
+app.use(cors());
+
+app.get('/api/news', async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM news');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({error: err.message});
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`server started on http://localhost:${PORT}`);
+});
+
+
