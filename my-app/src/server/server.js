985abd86b10f27c4f22c3fd807b2a3915e8e8024
손성준 <sonsj97@gmail.com>
2023-08-18 01:50:00 +0900

    modify server.js

--- my-app/src/server/server.js
+++ my-app/src/server/server.js
@@ -1,26 +1,31 @@
 const express = require("express");
-const {Pool} = require('pg');
+const sql = require("mssql");
 const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-const pool = new Pool({
-    user: "postgres",
-    host: "192.168.0.102",
-    database: "postgres",
-    password: "qwaszx2689!",
-    port: 5432
-});
+// MSSQL 연결 설정
+const config = {
+    server: 'sql19-001.cafe24.com', // 데이터베이스 서버 주소
+    database: 'sonbs99', // 데이터베이스 이름
+    user: 'sonbs99', // 데이터베이스 유저 이름
+    password: 'dltjsgml!1', // 데이터베이스 유저 비밀번호
+    options: {
+        encrypt: true, // 암호화 사용 여부
+        trustServerCertificate: true // 서버 인증서 검증 여부
+    }
+};
 
 app.use(cors());
 
 app.get('/api/news', async (req, res) => {
     try {
-        const result = await pool.query('SELECT * FROM news');
-        res.json(result.rows);
+        const pool = await sql.connect(config);
+        const result = await pool.request().query('SELECT * FROM news');
+        res.json(result.recordset); // recordset으로 결과를 JSON 형태로 변환하여 응답
     } catch (err) {
         console.error(err);
-        res.status(500).json({error: err.message});
+        res.status(500).json({ error: err.message });
     }
 });
 
