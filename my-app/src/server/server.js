0557f7b5e9f5744f70be69d811c73b3abb02f6d7
손성준 <sonsj97@gmail.com>
2023-08-18 02:13:55 +0900

    add dbconfig

--- my-app/src/server/server.js
+++ my-app/src/server/server.js
@@ -1,28 +1,20 @@
+// server.js
+
 const express = require("express");
 const sql = require("mssql");
 const cors = require('cors');
+const dbConfig = require('./dbConfig'); // dbConfig.js 파일에서 접속 정보 가져오기
+
 const app = express();
 const PORT = 5000;
-
-// MSSQL 연결 설정
-const config = {
-    server: 'sql19-001.cafe24.com', // 데이터베이스 서버 주소
-    database: 'sonbs99', // 데이터베이스 이름
-    user: 'sonbs99', // 데이터베이스 유저 이름
-    password: 'dltjsgml!1', // 데이터베이스 유저 비밀번호
-    options: {
-        encrypt: true, // 암호화 사용 여부
-        trustServerCertificate: true // 서버 인증서 검증 여부
-    }
-};
 
 app.use(cors());
 
 app.get('/api/news', async (req, res) => {
     try {
-        const pool = await sql.connect(config);
+        const pool = await sql.connect(dbConfig); // dbConfig에서 가져온 접속 정보 사용
         const result = await pool.request().query('SELECT * FROM news');
-        res.json(result.recordset); // recordset으로 결과를 JSON 형태로 변환하여 응답
+        res.json(result.recordset);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: err.message });
