67c65fda66a558a02696449e5568d5c8839cacef
SoengJoon <sonsj97@gmail.com>
2023-08-19 23:53:56 +0900

    news db connect

--- pyfile/news.py
+++ pyfile/news.py
@@ -1,7 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-# ,
-categories = ['society', 'politics', 'economic']
+import pyodbc
+
+
+server_name = "sql19-001.cafe24.com"
+db_name = "sonbs99"
+user_id = "sonbs99"
+password = "dltjsgml!1"
+
+# 연결 설정
+conn = pyodbc.connect(
+    f"DRIVER=ODBC Driver 17 for SQL Server;SERVER={server_name};DATABASE={db_name};UID={user_id};PWD={password}"
+)
+cursor = conn.cursor()
+
+
 def get_newslist(category):
     url = f"https://news.daum.net/breakingnews/{category}"
     html = requests.get(url, headers={"User-Agent": "Mozilla/5.0" \
@@ -10,9 +23,10 @@ def get_newslist(category):
     soup = BeautifulSoup(html.text, 'html.parser')
     list_news2 = soup.find('ul', class_="list_news2 list_allnews")
     newslist = list_news2.find_all('div', class_="cont_thumb")
+    get_news(newslist, category)
     return newslist
 
-def get_news(newslist):
+def get_news(newslist, category):
     for news in newslist:
         title_info = news.find('strong', class_="tit_thumb")
         title = title_info.find('a').text
@@ -21,21 +35,40 @@ def get_news(newslist):
         press = press_info.split()[0]
         date = press_info.split()[2]
 
+        content_info = news.find('div', class_="desc_thumb")
+        content = content_info.find('span', class_="link_txt").text
+
         news_info = {
             'title': title,
             'press': press,
+            'content': content,
             'date': date,
             'newstype': category
         }
-        print(news_info)
+        save_news_to_db(news_info)
+
+
+def save_news_to_db(news_info):
+        title = news_info['title']
+        press = news_info['press']
+        content = news_info['content']
+        date = news_info['date']
+        newstype = news_info['newstype']
+
+        # SQL 쿼리 작성
+        query = f"INSERT INTO news (title, press, content, date, newstype) VALUES (?, ?,?, ?, ?)"
+
+        # 쿼리 실행
+        cursor.execute(query, (title, press, content, date, newstype))
+        conn.commit()
+
 
 
 ####print(tag_lst = ex_tag(100, 1))
 if __name__ == "__main__":
-    page = 1
+    categories = ['society', 'politics', 'economic']
     for category in categories:
         one_list = get_newslist(category)
-        get_news(one_list)
 
 
 
