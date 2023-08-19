b7ecdb8571c878de1979e1d4e8e396c0fbc68012
SoengJoon <sonsj97@gmail.com>
2023-08-19 21:32:39 +0900

    들여쓰기 조정

--- pyfile/news.py
+++ pyfile/news.py
@@ -1,25 +1,24 @@
 import requests
-from bs4 import BeautifulSoup 
-from tqdm.notebook import tqdm
+from bs4 import BeautifulSoup
+
 
 def ex_tag(sid, page):
-
-    url = f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={sid}"\
-    "#&date=%2000:00:00&page={page}"
-    html = requests.get(url, headers={"User-Agent": "Mozilla/5.0"\
-    "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "\
-    "Chrome/110.0.0.0 Safari/537.36"})
+    url = f"https://news.naver.com/main/main.naver?mode=LSD&mid=shm&sid1={sid}" \
+          "#&date=%2000:00:00&page={page}"
+    html = requests.get(url, headers={"User-Agent": "Mozilla/5.0" \
+                                                    "(Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+                                                    "Chrome/110.0.0.0 Safari/537.36"})
     soup = BeautifulSoup(html.text, 'html.parser')
     a_tag = soup.find_all("a")
-    
-    
+
     tag_lst = []
     for a in a_tag:
-        if "href" in a.attrs:  
+        if "href" in a.attrs:
             if (f"sid={sid}" in a["href"]) and ("article" in a["href"]):
                 tag_lst.append(a["href"])
-                
+
     return tag_lst
+
 
 ####print(tag_lst = ex_tag(100, 1))
 if __name__ == "__main__":
@@ -28,6 +27,3 @@ if __name__ == "__main__":
     links = ex_tag(sid, page)
     for link in links:
         print(link)
-    
-     ##################
-     
