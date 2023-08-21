f4de83e04ae2ada66b266948b456483a2d42d13a
손성준 <sonsj97@gmail.com>
2023-08-21 13:35:40 +0900

    db연결 조작

--- pyfile/news.py
+++ pyfile/news.py
@@ -18,7 +18,6 @@ conn = pyodbc.connect(
 )
 cursor = conn.cursor()
 
-
 def get_newslist(category):
     url = f"https://news.daum.net/breakingnews/{category}"
     html = requests.get(url, headers={"User-Agent": "Mozilla/5.0" \
@@ -27,54 +26,48 @@ def get_newslist(category):
     soup = BeautifulSoup(html.text, 'html.parser')
     list_news2 = soup.find('ul', class_="list_news2 list_allnews")
     newslist = list_news2.find_all('div', class_="cont_thumb")
-    get_news(newslist, category)
     return newslist
 
-def get_news(newslist, category):
-    for news in newslist:
-        title_info = news.find('strong', class_="tit_thumb")
-        title = title_info.find('a').text
-
-        press_info = news.find('span', class_="info_news").text
-        press = press_info.split()[0]
-        date = press_info.split()[2]
-
-        content_info = news.find('div', class_="desc_thumb")
-        content = content_info.find('span', class_="link_txt").text
-
-        news_info = {
-            'title': title,
-            'press': press,
-            'content': content,
-            'date': date,
-            'newstype': category
-        }
-        save_news_to_db(news_info)
-
-
 def save_news_to_db(news_info):
-        title = news_info['title']
-        press = news_info['press']
-        content = news_info['content']
-        date = news_info['date']
-        newstype = news_info['newstype']
+    title = news_info['title']
+    press = news_info['press']
+    content = news_info['content']
+    date = news_info['date']
+    newstype = news_info['newstype']
 
-        # SQL 쿼리 작성
-        query = f"INSERT INTO news (title, press, content, date, newstype) VALUES (?, ?, ?, ?, ?)"
+    # SQL 쿼리 작성
+    query = "INSERT INTO news (title, press, content, date, newstype) VALUES (?, ?, ?, ?, ?)"
 
-        # 쿼리 실행
-        cursor.execute(query, (title, press, content, date, newstype))
-        conn.commit()
+    # 쿼리 실행
+    cursor.execute(query, (title, press, content, date, newstype))
+    conn.commit()
 
-
-
-####print(tag_lst = ex_tag(100, 1))
-if __name__ == "__main__":
+def main():
     categories = ['society', 'politics', 'economic']
+
     for category in categories:
-        one_list = get_newslist(category)
+        newslist = get_newslist(category)
 
+        for news in newslist:
+            title_info = news.find('strong', class_="tit_thumb")
+            title = title_info.find('a').text
 
+            press_info = news.find('span', class_="info_news").text
+            press = press_info.split()[0]
+            date = press_info.split()[2]
 
+            content_info = news.find('div', class_="desc_thumb")
+            content = content_info.find('span', class_="link_txt").text
 
+            news_info = {
+                'title': title,
+                'press': press,
+                'content': content,
+                'date': date,
+                'newstype': category
+            }
 
+            save_news_to_db(news_info)
+
+if __name__ == "__main__":
+    main()
