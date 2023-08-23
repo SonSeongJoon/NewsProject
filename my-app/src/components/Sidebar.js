19eb412dedab83d09841c5a5ccfdb8dd55d91a9d
손유영 <sonyouyoung27@gmail.com>
2023-08-23 21:43:20 +0900

    sidebar js,css 수정,icons파일 추가하여 closeicon.js 만듦

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,25 +1,54 @@
 // Sidebar.js
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from "react";
+import classNames from "classnames";
+import styles from "./sidebar.module.css";
+import CloseIcon from "./icons/CloseIcon"; // 닫기 아이콘 SVG 컴포넌트를 불러온다.
 
-function Sidebar() {
-    const categories = ['Dashboard', 'society', 'politics', 'economic', 'foreign'];
-    return (
-        <div className='mt-5'>
-            <ul>
-                {categories.map(category => (
-                    <li key={category} className="mb-2">
-                        <Link
-                            to={category === 'Dashboard' ? '/'.toLowerCase() : `/${category.toLowerCase()}`}
-                            className="text-gray-700 text-xl font-bold hover:underline"
-                        >
-                            {category}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
-}
+const Sidebar = ({ width = 280, children }) => {
+  const [isOpen, setOpen] = useState(false);
+  const [xPosition, setX] = useState(-width);
+  const side = useRef();
+
+  const toggleMenu = () => {
+    if (xPosition < 0) {
+      setX(0);
+      setOpen(true);
+    } else {
+      setX(-width);
+      setOpen(false);
+    }
+  };
+
+  const handleClose = (e) => {
+    const sideArea = side.current;
+    const isOutsideClick = !sideArea || !sideArea.contains(e.target);
+    if (isOpen && isOutsideClick) {
+      setX(-width);
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleClose);
+    return () => {
+      window.removeEventListener("click", handleClose);
+    };
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <div
+        ref={side}
+        className={classNames(styles.sidebar, { [styles.open]: isOpen })}
+        style={{ width: `${width}px`, transform: `translateX(${-xPosition}px)` }}
+      >
+        <button onClick={toggleMenu} className={styles.button}>
+          {isOpen ? <CloseIcon /> : <img src="images/avatar.png" alt="contact open button" className={styles.openBtn} />}
+        </button>
+        <div className={styles.content}>{children}</div>
+      </div>
+    </div>
+  );
+};
 
 export default Sidebar;
