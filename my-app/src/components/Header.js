c3411e1662c95cd76c015bb86df853b9f3a376d4
손성준 <sonsj97@gmail.com>
2023-08-28 16:51:14 +0900

    change projectName

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -2,7 +2,7 @@ function Header() {
     return (
         <>
             <header className='flex item-center pt-3'>
-                <h1 className='text-3xl font-bold'>News Project</h1>
+                <h1 className='text-3xl font-bold'>NEWS TECK</h1>
             </header>
         </>
     )
