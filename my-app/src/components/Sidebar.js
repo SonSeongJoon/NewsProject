eb68f0a842692067a22e91e162562e12a881d5d4
손성준 <sonsj97@gmail.com>
2023-08-28 16:51:45 +0900

    카테고리 상태관리를 위한 통합화

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,16 +1,17 @@
 // Sidebar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {categories} from "./Categories";
 
 function Sidebar() {
-  const categories = ['Dashboard', 'society', 'politics', 'economic', 'foreign', 'culture', 'entertain', 'sports', 'digital', 'editorial', 'press', 'botnews'];
+    const dashboard = categories[0];
   return (
       <div className='mt-5'>
         <ul>
           {categories.map(category => (
               <li key={category} className="mb-2">
                 <Link
-                    to={category === 'Dashboard' ? '/'.toLowerCase() : `/${category.toLowerCase()}`}
+                    to={category === dashboard ? '/'.toLowerCase() : `/${category.toLowerCase()}`}
                     className="text-gray-700 text-xl font-bold hover:underline"
                 >
                   {category}
