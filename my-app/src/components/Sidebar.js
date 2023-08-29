9ff0c177fd021e348a539dddc7e7b66986c4c0c0
sonsj <sonsj97@hanyang.ac.kr>
2023-08-29 12:14:14 +0900

    :lipstick: 사이드바 ui 작업 한 블럭 완료

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,25 +1,37 @@
-// Sidebar.js
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {categories} from "./Categories";
+import { categories } from "./Categories";
+import { BsFillCaretDownFill, BsNewspaper, BsDot } from "react-icons/bs";
 
 function Sidebar() {
+    const [isOpen, setIsOpen] = React.useState(false);
     const dashboard = categories[0];
-  return (
-      <div className='mt-5'>
-        <ul>
-          {categories.map(category => (
-              <li key={category} className="mb-2">
-                <Link
-                    to={category === dashboard ? '/'.toLowerCase() : `/${category.toLowerCase()}`}
-                    className="text-gray-700 text-xl font-bold hover:underline"
-                >
-                  {category}
-                </Link>
-              </li>
-          ))}
-        </ul>
-      </div>
-  );
+    return (
+        <div className='mt-5'>
+            <ul>
+                <div className='flex items-center border-2 justify-between'>
+                    <div className='flex items-center'>
+                        <BsNewspaper className='mx-2' size={20}/>
+                        <button className='text-gray-700 text-xl font-bold' onClick={() => setIsOpen(!isOpen)}>뉴스</button>
+                    </div>
+                    <BsFillCaretDownFill className='mr-2' size={20} color='gray' />
+                </div>
+                <div className='ml-9'>
+                    {isOpen && categories.map(category => (
+                        <li key={category} className="flex items-center my-1">
+                            <BsDot/>
+                            <Link
+                                to={category === dashboard ? '/'.toLowerCase() : `/${category.toLowerCase()}`}
+                                className="text-gray-700 text-md hover:underline"
+                            >
+                                {category}
+                            </Link>
+                        </li>
+                    ))}
+                </div>
+            </ul>
+        </div>
+    );
 }
+
 export default Sidebar;
