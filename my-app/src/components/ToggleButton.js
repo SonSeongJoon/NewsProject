3078fcd783c494e4f394bea414c1610359f2af08
sonsj <sonsj97@hanyang.ac.kr>
2023-08-29 13:37:18 +0900

    :lipstick: 토글 컴포넌트 생성

---
+++ my-app/src/components/ToggleButton.js
@@ -0,0 +1,9 @@
+import {BsChevronRight} from "react-icons/bs";
+
+export function ToggleButton({isOpen}) {
+    return (
+        <div className='mr-2'>
+            <BsChevronRight size={15} color='gray' className={`transition-transform duration-300 ${isOpen ? 'transform rotate-90' : ''}`}/>
+        </div>
+    )
+}
