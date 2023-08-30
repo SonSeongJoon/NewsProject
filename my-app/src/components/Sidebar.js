c06325d3203c3ad37974bd726f7ea342ff0a01a8
sonsj <sonsj97@hanyang.ac.kr>
2023-08-30 09:40:01 +0900

    :sparkles: Sidebar에 구성되는 메뉴를 따로 폴더로 관리

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,29 +1,11 @@
 import React from 'react';
-import {categories} from "./Categories";
-import {BsNewspaper} from "react-icons/bs";
-import {ToggleButton} from "./ToggleButton";
-import {NewsCategoryItem} from "./NewsCategoryItem";
+import {SidebarCategory} from "./SidebarList/SidebarCategory";
 
 function Sidebar() {
-    const [isOpen, setIsOpen] = React.useState(false);
-    const dashboard = categories[0];
     return (
         <div className='mt-5'>
             <ul>
-                <div className='flex p-1 items-center justify-between cursor-pointer'
-                     onClick={() => setIsOpen(!isOpen)}>
-                    <div className='flex items-center'>
-                        <BsNewspaper className='mx-2' size={20}/>
-                        <button className='text-gray-700 text-xl font-bold'>뉴스</button>
-                    </div>
-                    <ToggleButton isOpen={isOpen}/>
-                </div>
-                <div
-                    className={`ml-9 overflow-hidden transition-all ease-out duration-700 ${isOpen ? 'max-h-[500px]' : 'max-h-0'}`}>
-                    {isOpen && categories.map(category => (
-                        <NewsCategoryItem key={category} category={category} Dashboard={dashboard}/>
-                    ))}
-                </div>
+                <SidebarCategory/>
             </ul>
         </div>
     );
---
+++ my-app/src/components/SidebarList/SidebarCategory.js
@@ -0,0 +1,30 @@
+import {BsNewspaper} from "react-icons/bs";
+import {ToggleButton} from "../ToggleButton";
+import {categories} from "../Categories";
+import {NewsCategoryItem} from "../NewsCategoryItem";
+import React from "react";
+
+export function SidebarCategory() {
+    const [isOpen, setIsOpen] = React.useState(false);
+    const dashboard = categories[0];
+  return (
+     <>
+         <div className='flex p-1 items-center justify-between cursor-pointer'
+              onClick={() => setIsOpen(!isOpen)}>
+             <div className='flex items-center'>
+                 <BsNewspaper className='mx-2' size={20}/>
+                 <button className='text-gray-700 text-xl font-bold'>뉴스</button>
+             </div>
+             <ToggleButton isOpen={isOpen}/>
+         </div>
+         <div
+             className={`ml-9 overflow-hidden transition-all ease-out duration-700 ${isOpen ? 'max-h-[500px]' : 'max-h-0'}`}>
+             {isOpen && categories.map(category => (
+                 <NewsCategoryItem key={category} category={category} Dashboard={dashboard}/>
+             ))}
+         </div>
+
+
+     </>
+  )
+}
